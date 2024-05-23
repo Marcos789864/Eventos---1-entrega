ef48feff0f6eab651646c102e6bc19b2979a1078
Android Studio <devandroid@ort>
2024-05-23 11:20:58 -0300

    se termino el events-repository

--- src/repositories/events-repository.js
+++ src/repositories/events-repository.js
@@ -4,12 +4,12 @@ const {Client, Pool } = pkg;
 
 export default class Events
 {
-    buscarEventoNombre = async (name) =>
+    getEventByName = async (name) =>
     {
         const client = new Client(DBConfig);
         try {
             await client.connect();
-            const sql = 'Select * FROM events WHERE name = $1';
+            const sql = 'SELECT * FROM events WHERE name = $1';
     
             const result = await client.query(sql, [name]);
             if (result.rowCount > 0) {
@@ -23,12 +23,12 @@ export default class Events
         return result;
     }
 
-    buscarEventoCategoria = async (category) =>
+    getEventByCategory = async (category) =>
     {
         const client = new Client(DBConfig);
         try {
             await client.connect();
-            const sql = 'Select * FROM event_categories WHERE name = $1';
+            const sql = 'SELECT * FROM events INNER JOIN event_categories ON events.id_event_category = event_categories.id WHERE event_categories.name = $1';
     
             const result = await client.query(sql, [category]);
             if (result.rowCount > 0) {
@@ -42,12 +42,12 @@ export default class Events
         return result;
     }
 
-    buscarEventoFecha = async (fecha) =>
+    getEventByDate = async (fecha) =>
     {
         const client = new Client(DBConfig);
         try {
             await client.connect();
-            const sql = 'Select * FROM events WHERE start_date = $1';
+            const sql = 'SELECT * FROM events WHERE start_date = $1';
             const result = await client.query(sql, [fecha]);
             if (result.rowCount > 0) {
                 success = true;
@@ -59,18 +59,33 @@ export default class Events
         }
         return result;
     }
-//fijarse de hacer los innerjoin
-    buscarEventoTag = async (tag) =>
+    getEventByTag = async (tag) =>
     {
         const client = new Client(DBConfig);
         try {
             await client.connect();
-            const sql = 'Select * FROM events WHERE tags = $1';
+            const sql = 'SELECT * FROM events INNER JOIN event_tags ON events.id = event_tags.id_event INNER JOIN tags ON event_tags.id_tag = tags.id WHERE tags.name = $1';
             const result = await client.query(sql, [tag]);
             if (result.rowCount > 0) {
                 success = true;
             }
-            
+            await client.end();
+        } catch (error) {
+            console.log(error);
+        }
+        return result;
+    }
+
+    getEventByTag = async (tag) =>
+    {
+        const client = new Client(DBConfig);
+        try {
+            await client.connect();
+            const sql = 'SELECT * FROM events INNER JOIN event_tags ON events.id = event_tags.id_event INNER JOIN tags ON event_tags.id_tag = tags.id INNER JOIN event_enrollments ON events.id = event.enrollments.id_event INNER JOIN users ON events.id_creator_user = users.id INNER JOIN event_categories ON events.id_event_category = event_categories.id INNER JOIN event_locations ON events.id_event_location = event_location.id INNER JOIN locations ON event_locations.id_location = locations.id INNER JOIN provinces ON locations.id_province = provinces.id';
+            const result = await client.query(sql, [tag]);
+            if (result.rowCount > 0) {
+                success = true;
+            }
             await client.end();
         } catch (error) {
             console.log(error);
