29cc1f010da92c28d7ef06a07383b616cf34aa5a
Android Studio <devandroid@ort>
2024-05-23 12:11:52 -0300

    a

--- src/repositories/events-repository.js
+++ src/repositories/events-repository.js
@@ -4,6 +4,24 @@ const {Client, Pool } = pkg;
 
 export default class Events
 {
+    getEvents = async () =>
+    {
+        const client = new Client(DBConfig);
+        try {
+            await client.connect();
+            const sql = 'SELECT * FROM events';
+    
+            const result = await client.query(sql);
+            if (result.rowCount > 0) {
+                success = true;
+            }
+            
+            await client.end();
+        } catch (error) {
+            console.log(error);
+        }
+        return result;
+    }
     getEventByName = async (name) =>
     {
         const client = new Client(DBConfig);
@@ -76,13 +94,61 @@ export default class Events
         return result;
     }
 
-    getEventByTag = async (tag) =>
+    getUsersFromEvent = async () =>
     {
         const client = new Client(DBConfig);
         try {
             await client.connect();
-            const sql = 'SELECT * FROM events INNER JOIN event_tags ON events.id = event_tags.id_event INNER JOIN tags ON event_tags.id_tag = tags.id INNER JOIN event_enrollments ON events.id = event.enrollments.id_event INNER JOIN users ON events.id_creator_user = users.id INNER JOIN event_categories ON events.id_event_category = event_categories.id INNER JOIN event_locations ON events.id_event_location = event_location.id INNER JOIN locations ON event_locations.id_location = locations.id INNER JOIN provinces ON locations.id_province = provinces.id';
-            const result = await client.query(sql, [tag]);
+            const sql = 'SELECT * FROM users INNER JOIN event_enrollments ON users.id = event_enrollments.id_user INNER JOIN events ON event_enrollments.id_event = events.id';
+            const result = await client.query(sql);
+            if (result.rowCount > 0) {
+                success = true;
+            }
+            await client.end();
+        } catch (error) {
+            console.log(error);
+        }
+        return result;
+    }
+    getUserFromEventByName = async (name) =>
+    {
+        const client = new Client(DBConfig);
+        try {
+            await client.connect();
+            const sql = 'SELECT * FROM users INNER JOIN event_enrollments ON users.id = event_enrollments.id_user INNER JOIN events ON event_enrollments.id_event = events.id WHERE users.first_name = $1';
+            const result = await client.query(sql, [name]);
+            if (result.rowCount > 0) {
+                success = true;
+            }
+            await client.end();
+        } catch (error) {
+            console.log(error);
+        }
+        return result;
+    }
+    getUserFromEventByLastName = async (last_name) =>
+    {
+        const client = new Client(DBConfig);
+        try {
+            await client.connect();
+            const sql = 'SELECT * FROM users INNER JOIN event_enrollments ON users.id = event_enrollments.id_user INNER JOIN events ON event_enrollments.id_event = events.id WHERE users.last_name = $1';
+            const result = await client.query(sql, [last_name]);
+            if (result.rowCount > 0) {
+                success = true;
+            }
+            await client.end();
+        } catch (error) {
+            console.log(error);
+        }
+        return result;
+    }
+    getUserFromEventByLastName = async (username) =>
+    {
+        const client = new Client(DBConfig);
+        try {
+            await client.connect();
+            const sql = 'SELECT * FROM users INNER JOIN event_enrollments ON users.id = event_enrollments.id_user INNER JOIN events ON event_enrollments.id_event = events.id WHERE users.username = $1';
+            const result = await client.query(sql, [username]);
             if (result.rowCount > 0) {
                 success = true;
             }
