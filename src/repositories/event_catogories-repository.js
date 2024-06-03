8c35254709f27be5994eb50e6880ca483a08822c
Android Studio <devandroid@ort>
2024-06-03 08:58:11 -0300

    jaja

--- src/repositories/event_catogories-repository.js
+++ src/repositories/event_catogories-repository.js
@@ -9,7 +9,22 @@ export default class events_categories
         const client = new Client(DBConfig);
         try {
             await client.connect();
-            const sql = 'SELECT * FROM event_locations where id_locations = $1';
+            const sql = 'SELECT * FROM event_categories';
+            const result = await client.query(sql);
+            if (result.rowCount > 0) {
+                success = true;
+            }
+            await client.end();
+        } catch (error) {
+            console.log(error);
+        }
+        return result;
+    }
+    getCateogoryById = async(id) =>{
+        const client = new Client(DBConfig);
+        try {
+            await client.connect();
+            const sql = 'SELECT * FROM event_categories WHERE id = $1';
             const result = await client.query(sql,[id]);
             if (result.rowCount > 0) {
                 success = true;
@@ -19,6 +34,37 @@ export default class events_categories
             console.log(error);
         }
         return result;
-
     }
+    createCategory = async(name,display_order) =>{
+        const client = new Client(DBConfig);
+        try {
+            await client.connect();
+            const sql = 'INSERT INTO event_categories(name,display_order) VALUES($1,$2)';
+            const result = await client.query(sql,[name,display_order]);
+            if (result.rowCount > 0) {
+                success = true;
+            }
+            await client.end();
+        } catch (error) {
+            console.log(error);
+        }
+        return result;
+    }
+    //FALTA EDITAR CATEGORIA
+    deleteCategory = async(id) =>{
+        const client = new Client(DBConfig);
+        try {
+            await client.connect();
+            const sql = 'DELETE FROM event_categories WHERE id = $1';
+            const result = await client.query(sql,[id]);
+            if (result.rowCount > 0) {
+                success = true;
+            }
+            await client.end();
+        } catch (error) {
+            console.log(error);
+        }
+        return result;
+    }
+
 }
